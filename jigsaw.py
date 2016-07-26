308a50d0277a005d08d0a2e8a70b853756de0e63
Ryan149 <ryanthorn@me.com>
2016-07-26 19:45:10 +0000

    git commit

---
+++ jigsaw.py
@@ -0,0 +1,10 @@
+f=open('/tmp/assemble','r')
+l=f.readlines()
+for z in range(0,185):
+ for i in range(0,z):
+  g=l[z]
+  h=l[i]
+  if g[i] in h[i]:
+   print "I win" 
+ for i in range(z+1,185):
+  print "yay"
