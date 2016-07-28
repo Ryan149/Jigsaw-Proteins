f8d2c6b42a716c89ea213a1fa20dccc4717d3522
Ryan149 <ryanthorn@me.com>
2016-07-28 20:19:58 +0000

    jigsaw-simple.py

--- jigsaw-first-four-letters.py
+++ jigsaw-first-four-letters.py
@@ -1,3 +1,16 @@
 f=open('/tmp/assemble','r')
 l=f.readlines()
-
+x=0
+y=1
+count1=0
+count2=0
+while y in range(0,185):
+ s1=l[x]
+ s2=l[y]
+ a=s1[count1],s1[count1+1],s1[count1+2],s1[count1+3]
+ b=s2[count2],s2[count2+1],s2[count2+2],s2[count2+3]
+ if (a==b):
+  
+ elif (a!=b):
+  solution=a
+  count2=count2+1
--- jigsaw-simple.py
+++ jigsaw-simple.py
@@ -1,4 +1,3 @@
-import re
 seq="sqtap","tapal","apalt"
 a=seq[0]
 b=seq[1]
@@ -6,13 +5,6 @@ c=seq[2]
 print a
 print b
 print c
-i=0
-g=5
-status="No_Match"
-while (status=="No_Match"):
- if (a[i]==b[g-2]) and (a[i+1]==b[g-1]) and (a[i+2]==b[g]):
-  print "Success"
-  status="Match"
- else:
-  i=i+1
-  print "Fail"
+print intersect(a,b)
+print intersect(b,c)
+print intersect(c,a)
